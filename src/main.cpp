334ae2d35016b29b3a08731a0840304565cea99a
Ethan Steinberg <ethan.steinberg@gmail.com>
2011-12-22 21:38:48 -0800

    Move all input to another method

--- src/main.cpp
+++ src/main.cpp
@@ -11,49 +11,11 @@
 
 namespace po = boost::program_options;
 
+boost::shared_ptr<ConfigurationManager> handleArgs(int argc, char** argv);
+
 int main(int argc, char** argv)
 {
-   po::positional_options_description p;
-   p.add("output-file",1);
-
-
-   po::options_description desc("Usage: screencap [OPTIONS] outputfile\nDefault output file is out.mkv");
-
-   po::options_description internal;
-   internal.add_options()
-   ("output-file",po::value<std::string>()->default_value("out.mkv"),"set output location");
-
-   po::options_description gen("General options");
-   gen.add_options()
-   ("help,h","Produce help message");
-
-   po::options_description video("Video output options");
-   video.add_options()
-   ("fps", po::value<int>()->default_value(30), "Video fps, needs to be an integer");
-
-   po::options_description other("Other options");
-   other.add_options()
-   ("temp-dir",po::value<std::string>()->default_value("/tmp"),"Location to store temporary files");
-
-   desc.add(gen);
-   desc.add(video);
-   desc.add(other);
-
-   po::variables_map vm;
-   po::store(po::command_line_parser(argc,argv).options(internal.add(desc)).positional(p).run(),vm);
-   po::notify(vm);
-
-   if (vm.count("help"))
-   {
-      std::cout<<desc<<"\n";
-      return 1;
-   }
-
-   auto configuration = ConfigurationManager::create();
-
-   configuration->setOption<int>("fps",vm["fps"].as<int>());
-   configuration->setOption<std::string>("tmpDir",vm["temp-dir"].as<std::string>());
-   configuration->setOption<std::string>("outFile",vm["output-file"].as<std::string>());
+   auto configuration = handleArgs(argc,argv);
 
 
    auto signalHandlerQueue = MessageQueue::create();
@@ -89,4 +51,51 @@ int main(int argc, char** argv)
    recieverThread.join();
    dumperThread.join();
    return 0;
+}
+
+boost::shared_ptr<ConfigurationManager> handleArgs(int argc, char** argv)
+{
+   po::positional_options_description p;
+   p.add("output-file",1);
+
+
+   po::options_description desc("Usage: screencap [OPTIONS] outputfile\nDefault output file is out.mkv");
+
+   po::options_description internal;
+   internal.add_options()
+   ("output-file",po::value<std::string>()->default_value("out.mkv"),"set output location");
+
+   po::options_description gen("General options");
+   gen.add_options()
+   ("help,h","Produce help message");
+
+   po::options_description video("Video output options");
+   video.add_options()
+   ("fps", po::value<int>()->default_value(30), "Video fps, needs to be an integer");
+
+   po::options_description other("Other options");
+   other.add_options()
+   ("temp-dir",po::value<std::string>()->default_value("/tmp"),"Location to store temporary files");
+
+   desc.add(gen);
+   desc.add(video);
+   desc.add(other);
+
+   po::variables_map vm;
+   po::store(po::command_line_parser(argc,argv).options(internal.add(desc)).positional(p).run(),vm);
+   po::notify(vm);
+
+   if (vm.count("help"))
+   {
+      std::cout<<desc<<"\n";
+      exit(1);
+   }
+
+   auto configuration = ConfigurationManager::create();
+
+   configuration->setOption<int>("fps",vm["fps"].as<int>());
+   configuration->setOption<std::string>("tmpDir",vm["temp-dir"].as<std::string>());
+   configuration->setOption<std::string>("outFile",vm["output-file"].as<std::string>());
+
+   return configuration;
 }
